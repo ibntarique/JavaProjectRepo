7dd9d34ffab4ddd14bf0072eaa56ec45f02741c3
Tarique <Tarique@Tarique-PC>
2017-11-05 14:09:46 +0530

    InitialCoomit

---
+++ Project1/src/Pac1/class1.java
@@ -0,0 +1,13 @@
+package Pac1;
+
+public class class1 {
+
+	public static void main(String[] args) {
+			
+		// TODO Auto-generated method stub
+		
+		System.out.println("TODO Auto-generated method stub");
+
+	}
+
+}
